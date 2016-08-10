492f898ec2aa60ab6a5ef4f13a465446b2b201a0
Gtsai13 <gtsai13@gmail.com>
2016-08-10 16:02:53 -0500

    Changing naming conventions

--- Join.py
+++ Join.py
@@ -5,27 +5,34 @@ from Spotfire.Dxp.Data import *
 from Spotfire.Dxp.Data import AddColumnsSettings, JoinType, DataColumnSignature, DataType
 from Spotfire.Dxp.Data.Import import DataTableDataSource
 
-ColumnA = DataColumnSignature(Document.Data.Tables["Daily Header"].Columns["API-14"])
-ColumnB = DataColumnSignature(Document.Data.Tables["DCA Parameters"].Columns["API-14"])
+#Define table references
+DCA_Parameters = Application.Document.Data.Tables["DCA Parameters"]
+Daily_Header = Application.Document.Data.Tables["Oxy Eag Daily Header"]
+
+
+ColumnA = DataColumnSignature(Daily_Header.Columns["PROPNUM"])
+ColumnB = DataColumnSignature(DCA_Parameters.Columns["PROPNUM"])
+
+
 ###Getting COlumn names for DCA Parameters table and all of the document propertys we want to save
-Oil_IP = Application.Document.Data.Tables["DCA Parameters"].Columns["Oil IP"].Name
-Oil_B = Application.Document.Data.Tables["DCA Parameters"].Columns["Oil B"].Name
-Oil_Di = Application.Document.Data.Tables["DCA Parameters"].Columns["Oil Di"].Name
-Oil_Dmin = Application.Document.Data.Tables["DCA Parameters"].Columns["Oil Dmin"].Name
-Oil_DaysFlat = Application.Document.Data.Tables["DCA Parameters"].Columns["Oil Days Flat"].Name
-Oil_Technical_EUR = Application.Document.Data.Tables["DCA Parameters"].Columns["Oil Technical EUR"].Name
-Gas_IP = Application.Document.Data.Tables["DCA Parameters"].Columns["Gas IP"].Name
-Gas_B = Application.Document.Data.Tables["DCA Parameters"].Columns["Gas B"].Name
-Gas_Di = Application.Document.Data.Tables["DCA Parameters"].Columns["Gas Di"].Name
-Gas_Dmin = Application.Document.Data.Tables["DCA Parameters"].Columns["Gas Dmin"].Name
-Gas_DaysFlat = Application.Document.Data.Tables["DCA Parameters"].Columns["Gas Days Flat"].Name
-Gas_Technical_EUR = Application.Document.Data.Tables["DCA Parameters"].Columns["Gas Technical EUR"].Name
-Cum_Oil_at_DaysFlat = Application.Document.Data.Tables["DCA Parameters"].Columns["Cum Oil at Days Flat"].Name
-Cum = Application.Document.Data.Tables["DCA Parameters"].Columns["Cum"].Name
-Months_Produced = Application.Document.Data.Tables["DCA Parameters"].Columns["Months Produced"].Name
-Commercial_EUR = Application.Document.Data.Tables["DCA Parameters"].Columns["Commerical EUR"].Name
-NPV10 = Application.Document.Data.Tables["DCA Parameters"].Columns["NPV-10"].Name
-IRR = Application.Document.Data.Tables["DCA Parameters"].Columns["IRR"].Name
+OilIP = DCA_Parameters.Columns["Oil IP"].Name
+OilB = DCA_Parameters.Columns["Oil B"].Name
+OilDi = DCA_Parameters.Columns["Oil Di"].Name
+OilDmin = DCA_Parameters.Columns["Oil Dmin"].Name
+DaysFlat = DCA_Parameters.Columns["Oil Days Flat"].Name
+OilTechnicalEUROne = DCA_Parameters.Columns["Oil Technical EUR"].Name
+GasIP = DCA_Parameters.Columns["Gas IP"].Name
+GasB = DCA_Parameters.Columns["Gas B"].Name
+GasDi = DCA_Parameters.Columns["Gas Di"].Name
+GasDmin = DCA_Parameters.Columns["Gas Dmin"].Name
+GasDaysFlat = DCA_Parameters.Columns["Gas Days Flat"].Name
+GasTechnicalEUROne = DCA_Parameters.Columns["Gas Technical EUR"].Name
+CumOilatDaysFlat = DCA_Parameters.Columns["Cum Oil at Days Flat"].Name
+Cum = DCA_Parameters.Columns["Cum"].Name
+MonthsProduced = DCA_Parameters.Columns["Months Produced"].Name
+CommercialEUR = DCA_Parameters.Columns["Commercial EUR"].Name
+NPV10 = DCA_Parameters.Columns["NPV-10"].Name
+IRRProperty = DCA_Parameters.Columns["IRR"].Name
 
 
 
@@ -33,70 +40,93 @@ IRR = Application.Document.Data.Tables["DCA Parameters"].Columns["IRR"].Name
 joinConditionMap = {ColumnA : ColumnB}
 ignoredCols = []
 
+ 
+
 #Remove oil DCA parameter colums before we readd them
-if Oil_IP == Application.Document.Data.Tables["Daily Header"].Columns["Oil IP"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Oil IP")
+if Daily_Header.Columns.Contains("Oil IP"):
+	if OilIP == Daily_Header.Columns["Oil IP"].Name:
+		Daily_Header.Columns.Remove("Oil IP")
 
-if Oil_B == Application.Document.Data.Tables["Daily Header"].Columns["Oil B"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Oil B")
+if Daily_Header.Columns.Contains("Oil B"):
+	if OilB == Daily_Header.Columns["Oil B"].Name:
+		Daily_Header.Columns.Remove("Oil B")
 
-if Oil_Di == Application.Document.Data.Tables["Daily Header"].Columns["Oil Di"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Oil Di")
+if Daily_Header.Columns.Contains("Oil Di"):
+	if OilDi == Daily_Header.Columns["Oil Di"].Name:
+		Daily_Header.Columns.Remove("Oil Di")
 
-if Oil_Dmin == Application.Document.Data.Tables["Daily Header"].Columns["Oil Dmin"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Oil Dmin")
+if Daily_Header.Columns.Contains("Oil Dmin"):
+	if OilDmin == Daily_Header.Columns["Oil Dmin"].Name:
+		Daily_Header.Columns.Remove("Oil Dmin")
 
-if Oil_DaysFlat == Application.Document.Data.Tables["Daily Header"].Columns["Oil Days Flat"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Oil Days Flat")
+if Daily_Header.Columns.Contains("Oil Days Flat"):
+	if DaysFlat == Daily_Header.Columns["Oil Days Flat"].Name:
+		Daily_Header.Columns.Remove("Oil Days Flat")
 
-if Oil_Technical_EUR == Application.Document.Data.Tables["Daily Header"].Columns["Oil Technical EUR"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Oil Technical EUR")
+if Daily_Header.Columns.Contains("Oil Technical EUR"):
+	if OilTechnicalEUROne == Daily_Header.Columns["Oil Technical EUR"].Name:
+		Daily_Header.Columns.Remove("Oil Technical EUR")
 
-#####Remove Gas Columns
-if Gas_IP == Application.Document.Data.Tables["Daily Header"].Columns["Gas IP"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Gas IP")
+	#####Remove Gas Columns
+if Daily_Header.Columns.Contains("Gas IP"):
+	if GasIP == Daily_Header.Columns["Gas IP"].Name:
+		Daily_Header.Columns.Remove("Gas IP")
 
-if Gas_B == Application.Document.Data.Tables["Daily Header"].Columns["Gas B"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Gas B")
+if Daily_Header.Columns.Contains("Gas B"):
+	if GasB == Daily_Header.Columns["Gas B"].Name:
+		Daily_Header.Columns.Remove("Gas B")
 
-if Gas_Di == Application.Document.Data.Tables["Daily Header"].Columns["Gas Di"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Gas Di")
+if Daily_Header.Columns.Contains("Gas Di"):
+	if GasDi == Daily_Header.Columns["Gas Di"].Name:
+		Daily_Header.Columns.Remove("Gas Di")
 
-if Gas_Dmin == Application.Document.Data.Tables["Daily Header"].Columns["Gas Dmin"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Gas Dmin")
+if Daily_Header.Columns.Contains("Gas Dmin"):
+	if GasDmin == Daily_Header.Columns["Gas Dmin"].Name:
+		Daily_Header.Columns.Remove("Gas Dmin")
 
-if Gas_DaysFlat == Application.Document.Data.Tables["Daily Header"].Columns["Gas Days Flat"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Gas Days Flat")
+if Daily_Header.Columns.Contains("Gas Days Flat"):
+	if GasDaysFlat == Daily_Header.Columns["Gas Days Flat"].Name:
+		Daily_Header.Columns.Remove("Gas Days Flat")
 
-if Gas_Technical_EUR == Application.Document.Data.Tables["Daily Header"].Columns["Gas Technical EUR"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Gas Technical EUR")
+if Daily_Header.Columns.Contains("Gas Technical EUR"):
+	if GasTechnicalEUROne == Daily_Header.Columns["Gas Technical EUR"].Name:
+		Daily_Header.Columns.Remove("Gas Technical EUR")
 
-####Rmove other miscellaneous columns
-if Cum_Oil_at_DaysFlat == Application.Document.Data.Tables["Daily Header"].Columns["Cum Oil at Days Flat"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Cum Oil at Days Flat")
+	####Rmove other miscellaneous columns
+if Daily_Header.Columns.Contains("Cum Oil at Days Flat"):	
+	if CumOilatDaysFlat == Daily_Header.Columns["Cum Oil at Days Flat"].Name:
+		Daily_Header.Columns.Remove("Cum Oil at Days Flat")
 
-if Cum == Application.Document.Data.Tables["Daily Header"].Columns["Cum"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Cum")
+if Daily_Header.Columns.Contains("Cum"):
+	if Cum == Daily_Header.Columns["Cum"].Name:
+		Daily_Header.Columns.Remove("Cum")
 
-if Months_Produced == Application.Document.Data.Tables["Daily Header"].Columns["Months Produced"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Months Produced")
+if Daily_Header.Columns.Contains("Months Produced"):
+	if MonthsProduced == Daily_Header.Columns["Months Produced"].Name:
+		Daily_Header.Columns.Remove("Months Produced")
 
-if Commercial_EUR == Application.Document.Data.Tables["Daily Header"].Columns["Commercial EUR"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("Commercial EUR")
+if Daily_Header.Columns.Contains("Commercial EUR"):
+	if CommercialEUR == Daily_Header.Columns["Commercial EUR"].Name:
+		Daily_Header.Columns.Remove("Commercial EUR")
 
-if NPV10 == Application.Document.Data.Tables["Daily Header"].Columns["NPV-10"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("NPV-10")
+if Daily_Header.Columns.Contains("NPV-10"):
+	if NPV10 == Daily_Header.Columns["NPV-10"].Name:
+		Daily_Header.Columns.Remove("NPV-10")
 
-if IRR == Application.Document.Data.Tables["Daily Header"].Columns["IRR"].Name:
-	Document.Data.Tables["Daily Header"].Columns.Remove("IRR")
+if Daily_Header.Columns.Contains("IRR"):
+	if IRRProperty == Daily_Header.Columns["IRR"].Name:
+		Daily_Header.Columns.Remove("IRR")
 
 #Remove B Value Column
 #if Column_IP == Application.Document.Data.Tables["Daily Header"].Columns["IP"].Name:
 	#Document.Data.Tables["Daily Header"].Columns.Remove("IP")
 
 settings = AddColumnsSettings( joinConditionMap, JoinType.LeftOuterJoin, ignoredCols)
-ds = DataTableDataSource(Document.Data.Tables["DCA Parameters"])
-Document.Data.Tables["Daily Header"].AddColumns(ds, settings)
-Document.Data.Tables["Daily Header"].Columns.Remove("Well Name (Header) (2)")
+ds = DataTableDataSource(DCA_Parameters)
+Daily_Header.AddColumns(ds, settings)
+
+##Remove if there is second column that we use to merge by
+if Daily_Header.Columns.Contains("PROPNUM (2)"):
+	Daily_Header.Columns.Remove("PROPNUM (2)")
 
 #this code joins columns from BakkenWells to Normalized Monthly Data
--- Save.py
+++ Save.py
@@ -6,14 +6,12 @@ from Spotfire.Dxp.Data import *
 #Get set of marked rows in data table
 dataTable = Document.Data.Tables["DCA Parameters"]
 rowIndexSet=Document.ActiveMarkingSelectionReference.GetSelection(dataTable).AsIndexSet()
-print rowIndexSet
-print rowIndexSet.Count
+
 
 #check that only 1 row is marked
 if rowIndexSet.Count == 1:
-	col1 = dataTable.Columns["API-14"].RowValues.GetFormattedValue(rowIndexSet.First)
-	print col1
-	col2 = dataTable.Columns["Well Name (Header)"].RowValues.GetFormattedValue(rowIndexSet.First)
+	col1 = dataTable.Columns["API"].RowValues.GetFormattedValue(rowIndexSet.First)
+	col2 = dataTable.Columns["PROPNUM"].RowValues.GetFormattedValue(rowIndexSet.First)
 	#dummy = dataTable.Columns["IP"].RowValues.GetFormattedValue(rowIndexSet.First)
 	#get current columns in data table, including dummy column  
 	#col2 = dataTable.Columns["Column 2"].RowValues.GetFormattedValue(rowIndexSet.First)
@@ -27,9 +25,9 @@ if rowIndexSet.Count == 1:
 
 	#Create replacement row using existing data plus our document property OilIP
 	#\r\n used for newline
-	#OilIp is a document property
-	data_for_new_row= str(col1) + "," + str(col2) + "," + str(Oil_IP)  + "," + str(Oil_B)  + "," + str(Oil_Di)  + "," + str(Oil_Dmin)  + "," + str(Oil_DaysFlat) + "," + str(Oil_Technical_EUR) + "," + str(Gas_IP) + "," + str(Gas_B)+ "," + str(Gas_Di)+ "," + str(Gas_Dmin)+ "," + str(Gas_DaysFlat)+ "," + str(Gas_Technical_EUR)+ "," + str(Cum_Oil_at_DaysFlat)+ "," + str(Cum) + "," + str(Months_Produced) + "," + str(Commercial_EUR) + "," + str(NPV10)+ "," + str(IRR) 
-	textData = "API-14, Well Name (Header), Oil IP, Oil B, Oil Di, Oil Dmin, Oil Days Flat, Oil Technical EUR, Gas IP, Gas B, Gas Di, Gas Dmin, Gas Days Flat, Gas Technical EUR, Cum Oil at Days Flat, Cum, Months Produced, Commercial EUR, NPV-10, IRR\r\n" + data_for_new_row + "\r\n"
+	#OilIP is a document property
+	data_for_new_row= str(col1)+ ","+ str(col2) + "," + str(OilIP)  + "," + str(OilB)  + "," + str(OilDi)  + "," + str(OilDmin)  + "," + str(DaysFlat) + "," + str(OilTechnicalEUROne) + "," + str(GasIP) + "," + str(GasB)+ "," + str(GasDi)+ "," + str(GasDmin)+ "," + str(GasDaysFlat)+ "," + str(GasTechnicalEUROne)+ "," + str(CumOilatDaysFlat)+ "," + str(Cum) + "," + str(MonthsProduced) + "," + str(CommercialEUR) + "," + str(NPV10)+ "," + str(IRRProperty) 
+	textData = "API,PROPNUM,Oil IP,Oil B,iOl Di,Oil Dmin,Oil Days Flat,Oil Technical EUR,Gas IP,Gas B,Gas Di,Gas Dmin,Gas Days Flat,Gas Technical EUR,Cum Oil at Days Flat,Cum,Months Produced,Commercial EUR,NPV-10,IRR\r\n" + data_for_new_row + "\r\n"
 
 	#Memory Stream stuff
 	stream = MemoryStream()
@@ -45,6 +43,29 @@ if rowIndexSet.Count == 1:
 	readerSettings.SetDataType(0, DataType.Real)
 	readerSettings.SetDataType(1, DataType.String)
 	readerSettings.SetDataType(2, DataType.Real)
+	readerSettings.SetDataType(3, DataType.Real)
+	readerSettings.SetDataType(4, DataType.Real)
+	readerSettings.SetDataType(5, DataType.Real)
+	readerSettings.SetDataType(6, DataType.Real)
+	readerSettings.SetDataType(7, DataType.Undefined)
+	readerSettings.SetDataType(8, DataType.Real)
+	readerSettings.SetDataType(9, DataType.Real)
+	readerSettings.SetDataType(11, DataType.Real)
+	readerSettings.SetDataType(12, DataType.Real)
+	readerSettings.SetDataType(13, DataType.Real)
+	readerSettings.SetDataType(14, DataType.Undefined)
+	readerSettings.SetDataType(15, DataType.Undefined)
+	readerSettings.SetDataType(16, DataType.Undefined)
+	readerSettings.SetDataType(17, DataType.Undefined)
+	readerSettings.SetDataType(18, DataType.String)
+	readerSettings.SetDataType(19, DataType.String)
+	readerSettings.SetDataType(20, DataType.Real)
+
+
+
+
+
+
 	textDataSource = TextFileDataSource(stream,readerSettings)
 	settings = AddRowsSettings(dataTable,textDataSource)
 	dataTable.AddRows(textDataSource,settings)
