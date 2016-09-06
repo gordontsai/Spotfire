3033e67de815b7010777f15eaecf12f7f4e38479
Gtsai13 <gtsai13@gmail.com>
2016-09-06 17:27:42 -0500

    Updated comments

--- CumOilAtDaysFlat.py
+++ CumOilAtDaysFlat.py
@@ -11,6 +11,43 @@ cursor = DataValueCursor.CreateFormatted(dataTable.Columns["Cum Oil At Days Flat
 
 
 
+
+"""c.Index or the .Index function in this case actually returns the row indexes for all marked rowIndexSet
+
 for c in dataTable.GetRows(rowIndexSet, cursor):
 	print c.Index
-		
+
+
+For example the output is below:
+23207
+23208
+23209
+23210
+23211
+23212
+"""		
+
+
+
+""" Trying to figure out what .CurrentValue does
+
+for c in dataTable.GetRows(rowIndexSet, cursor):
+	rowIndex = c.Index
+	print cursor.CurrentValue
+
+
+it actually returns a column of the current value for all of the row indexes for the markes rowIndexSet
+Example output:
+0.00
+(Empty)
+(Empty)
+(Empty)
+(Empty)
+(Empty)
+
+"""
+
+
+for c in dataTable.GetRows(rowIndexSet, cursor):
+	rowIndex = c.Index
+	if int(column)
